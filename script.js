9366ce86c8aec440ec94226bbe17e58b536beb4d
iqsnider <iqsnider@gmail.com>
2023-11-03 18:47:42 -0500

    quote api

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const dontworryaboutit = 5
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+
+function getRandomQuote() {
+    return fetch(RANDOM_QUOTE_API_URL)
+        .then(response => response.json())
+        .then(data => data.content)
+}
+
+async function renderNewQuote() {
+    const quote = await getRandomQuote()
+    quoteDisplayElement.innerText = quote
+}
+
+renderNewQuote()
